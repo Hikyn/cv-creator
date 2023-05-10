e60f02ae5d9f95e683f13b890e21f4188326dfbf
Danila <danila10danila@mail.ru>
2023-05-10 12:34:09 +0300

    CV components changed to functional

--- src/components/CVEditor.js
+++ src/components/CVEditor.js
@@ -1,36 +1,31 @@
-import { Component } from "react";
 import '../styles/CVEditor.css';
 import General from './General';
 import Educational from './Educational';
 import Practical from './Practical';
 
-export default class CVEditor extends Component {
-    constructor(props) {
-        super(props)
-    }
-
-    render() {
-        return (
-            <div className="CVEditor">
-                <General 
-                    setName={this.props.setName} 
-                    setEmail={this.props.setEmail} 
-                    setPhoneNumber={this.props.setPhoneNumber}
-                    setDescription={this.props.setDescription}
-                />
-                <Educational 
-                    setSchoolName={this.props.setSchoolName}
-                    setStudyTitle={this.props.setStudyTitle}
-                    setDateOfStudy={this.props.setDateOfStudy}
-                />
-                <Practical 
-                    setCompanyName={this.props.setCompanyName}
-                    setPositionTitle={this.props.setPositionTitle}
-                    setMainTasks={this.props.setMainTasks}
-                    setDateFrom={this.props.setDateFrom}
-                    setDateUntil={this.props.setDateUntil}
-                />
-            </div>
-        )
-    }
+function CVEditor(props) {
+    return (
+        <div className="CVEditor">
+            <General 
+                setName={props.setName} 
+                setEmail={props.setEmail} 
+                setPhoneNumber={props.setPhoneNumber}
+                setDescription={props.setDescription}
+            />
+            <Educational 
+                setSchoolName={props.setSchoolName}
+                setStudyTitle={props.setStudyTitle}
+                setDateOfStudy={props.setDateOfStudy}
+            />
+            <Practical 
+                setCompanyName={props.setCompanyName}
+                setPositionTitle={props.setPositionTitle}
+                setMainTasks={props.setMainTasks}
+                setDateFrom={props.setDateFrom}
+                setDateUntil={props.setDateUntil}
+            />
+        </div>
+    )
 }
+
+export default CVEditor;
--- src/components/CVPreview.js
+++ src/components/CVPreview.js
@@ -2,37 +2,33 @@ import { Component } from "react";
 import '../styles/CVPreview.css';
 import Section from "./Section";
 
-export default class CVPreview extends Component {
-    constructor(props) {
-        super(props)
-    }
-
-    render() {
-        return (
-            <div className="CVPreview">
-                <h1 className="name">{this.props.generalInfo.name}</h1>
-                <h4 className="contact-details">
-                    {this.props.generalInfo.phoneNumber} | <a href={'mailto:'+ this.props.generalInfo.email}>{this.props.generalInfo.email}</a>
-                </h4>
-                <div className="profile-description">{this.props.generalInfo.description}</div>
-                <Section name={'Education'} />
-                <div className="flexbox">
-                    <div>
-                        <div className="title">{this.props.educationalInfo.schoolName}</div>
-                        <div className="description">{this.props.educationalInfo.studyTitle}</div>
-                    </div>
-                    <div className="date">{this.props.educationalInfo.dateOfStudy}</div>
+function CVPreview(props) {
+    return (
+        <div className="CVPreview">
+            <h1 className="name">{props.generalInfo.name}</h1>
+            <h4 className="contact-details">
+                {props.generalInfo.phoneNumber} | <a href={'mailto:'+ props.generalInfo.email}>{props.generalInfo.email}</a>
+            </h4>
+            <div className="profile-description">{props.generalInfo.description}</div>
+            <Section name={'Education'} />
+            <div className="flexbox">
+                <div>
+                    <div className="title">{props.educationalInfo.schoolName}</div>
+                    <div className="description">{props.educationalInfo.studyTitle}</div>
                 </div>
-                <Section name={'Work experience'} />
-                <div className="flexbox">
-                    <div>
-                        <div className="title">{this.props.practicalInfo.companyName}</div>
-                        <div className="description">{this.props.practicalInfo.positionTitle}</div>
-                        <div className="description-detailed">{this.props.practicalInfo.mainTasks}</div>
-                    </div>
-                    <div className="date">{this.props.practicalInfo.dateFrom} <br></br> {this.props.practicalInfo.dateUntil}</div>
-                </div>
+                <div className="date">{props.educationalInfo.dateOfStudy}</div>
             </div>
-        )
-    }
+            <Section name={'Work experience'} />
+            <div className="flexbox">
+                <div>
+                    <div className="title">{props.practicalInfo.companyName}</div>
+                    <div className="description">{props.practicalInfo.positionTitle}</div>
+                    <div className="description-detailed">{props.practicalInfo.mainTasks}</div>
+                </div>
+                <div className="date">{props.practicalInfo.dateFrom} <br></br> {props.practicalInfo.dateUntil}</div>
+            </div>
+        </div>
+    )
 }
+
+export default CVPreview;
